77773ff3336e0d92bb3bedf0d270a9e10a8cf5a9
sumch76 <sumitchaubey76@gmail.com>
2024-10-22 18:14:15 +0530

    get

--- server/routes/UserAuthRouter.js
+++ server/routes/UserAuthRouter.js
@@ -3,11 +3,12 @@ const {
   signup,
   login,
   logout,
+  get,
   myDetails,
 } = require("../controllers/UserAuthController");
 const { verifyToken } = require("../middlewares/verifytoken");
 const UserAuthRouter = express.Router();
-
+UserAuthRouter.get("/",get)
 UserAuthRouter.post("/signup", signup);
 UserAuthRouter.post("/login", login);
 UserAuthRouter.post("/logout", logout);
